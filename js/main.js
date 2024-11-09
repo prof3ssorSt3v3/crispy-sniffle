3ac06d3b3affefd5bf883d40beaad2634bccb10a
Prof3ssorSt3v3 <griffis@algonquincollege.com>
2024-11-09 16:44:53 -0500

    starter files

---
+++ js/main.js
@@ -0,0 +1,29 @@
+const storageKey = 'myUniqueStorageKey';
+let movieList = [];
+
+(() => {
+  //init function
+  //form submit listener
+  //storage listener
+})();
+
+function handleSubmit(ev) {
+  ev.preventDefault();
+}
+
+function handleStorage(ev) {
+  //if storage has been updated on another tab then
+  //update the display
+}
+
+function saveToStorage() {
+  //update the movieList in storage
+}
+
+function readFromStorage() {
+  //retrieve the contents from storage
+}
+
+function buildList(list) {
+  //build contents of <dl> from array of objects
+}
